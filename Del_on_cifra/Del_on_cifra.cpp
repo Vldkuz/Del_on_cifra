a1599ae589a9412af3c7364ec37b040c2483e9a3
Vladislav <kuznetsov230304@gmail.com>
2022-10-21 00:11:50 +0500

    Добавьте файлы проекта.

---
+++ Del_on_cifra/Del_on_cifra.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    int k = n;
+    while (n > 0)
+    {
+        int t = n % 10;
+        if (t != 0 && k % t == 0)
+        {
+            cout << "Y\n";
+            return 0;
+        }
+        n /= 10;
+    }
+    cout << "N\n";
+    return 0;
+}
+
